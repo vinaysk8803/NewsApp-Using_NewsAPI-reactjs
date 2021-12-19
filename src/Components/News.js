dfb39f605c9c70369438d8b237c1b0bd4e6ff7cc
Vinay <vinaysinghvsk8803@gmail.com>
2021-12-19 13:19:47 +0530

    till video 33

---
+++ src/Components/News.js
@@ -0,0 +1,148 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
+
+export class News extends Component {
+  static defaultProps = {
+    country: "in",
+    pageSize: 8,
+    category: "general",
+  };
+  static propType = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+    document.title = `${this.props.category} - NewsDekho;
+  }
+
+  async updateNews() {
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=38f868d724684ae79fad3fe633f449ec&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({ loading: true });
+    let data = await fetch(url);
+    let parseData = await data.json();
+
+    this.setState({
+      articles: parseData.articles,
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
+  }
+
+  async componentDidMount() {
+    // let url =
+    //   `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=38f868d724684ae79fad3fe633f449ec&page=1&pageSize=${this.props.pageSize}`;
+    //   this.setState({loading:true});
+    //   let data = await fetch(url);
+    // let parseData = await data.json();
+
+    // this.setState({ articles: parseData.articles,
+    //   totalResults:parseData.totalResults,
+    //   loading:false
+    //  })
+
+    this.updateNews();
+  }
+
+  handlePrevClick = async () => {
+    //   let url =
+    //   `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=38f868d724684ae79fad3fe633f449ec&page=${this.state.page -1}&pageSize=${this.props.pageSize}`;
+
+    //   this.setState({loading:true});
+    //   let data = await fetch(url);
+    // let parseData = await data.json();
+
+    
+    //   this.setState({
+    //     page: this.state.page-1,
+    //     articles: parseData.articles,
+    //     loading: false
+
+    //   })
+
+    this.setState({ page: this.state.page - 1 });
+    this.updateNews();
+  };
+
+  handleNextClick = async () => {
+    // if(!(this.state.page + 1> Math.ceil(this.state.totalResults/this.props.pageSize))){
+    //     let url =
+    //     `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=38f868d724684ae79fad3fe633f449ec&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
+    //     this.setState({loading:true});
+    //     let data = await fetch(url);
+    //   let parseData = await data.json();
+    //     this.setState({
+    //       page: this.state.page+1,
+    //       articles: parseData.articles,
+    //       loading: false
+
+    //     })
+    //   }
+
+    this.setState({ page: this.state.page + 1 });
+    this.updateNews();
+  };
+
+  render() {
+    return (
+      <div className="container my-3 ">
+        <h1 className="text-center" style={{ margin: "35px 0px" }}>
+          NewsDekho - Top headLines
+        </h1>
+        {this.state.loading && <Spinner />}
+
+        <div className="row">
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={element.title ? element.title : ""}
+                    description={element.description ? element.description : ""}
+                    imageurl={element.urlToImage}
+                    newsurl={element.url}
+                    author={element.author}
+                    date={element.publishedAt}
+                    source={element.source.name}
+                  />
+                </div>
+              );
+            })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
+          </button>
+          <h5>Made By - Vinay Singh Kaintura using News API</h5>
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
